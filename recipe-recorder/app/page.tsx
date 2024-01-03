0ab555d046d5646f4d312b7072eb4cebf58d1ece
ttashatss <tasha.tanarugs@gmail.com>
2024-01-03 20:54:41 +0100

    add login

--- recipe-recorder/app/page.tsx
+++ recipe-recorder/app/page.tsx
@@ -2,17 +2,22 @@
 
 import Image from 'next/image'
 import Button from "../components/Button";
-import { useRouter } from 'next/navigation'
+import Header from "../components/Header";
+import { useRouter } from 'next/navigation';
 import { clients } from '@clerk/clerk-sdk-node';
+ 
 
 
 export default function Home() {
   const router = useRouter()
   return (
     <main>
-      <div>
+      <div className='body'>
         <h1>Embark on your culinary journey!</h1>
-        <Button text={"Log In"} onClick={undefined}/>
+        <p className='body-text'>Log in now to craft and capture your delectable recipes.</p>
+        <div className='log-in'>
+          <Button text={"Log In"} onClick={() => router.push('/recipe')}/>
+        </div>
       </div>
     </main>
   )
--- recipe-recorder/app/recipe/page.tsx
+++ recipe-recorder/app/recipe/page.tsx
@@ -1,8 +1,17 @@
-const RecipePage = () => {
+import { currentUser } from "@clerk/nextjs";
+import Button from "@/components/Button";
+
+const RecipePage = async () => {
+  const user = await currentUser();
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <div className='body'>
+        <h1>{user?.firstName}'s Recipes</h1>
+        <div className='add-recipe'>
+          <Button text={"Add Recipe"} onClick={undefined}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
